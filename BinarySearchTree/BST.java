2b59b7a45e5dd8d82e8d4e9fc0f3f9190ce3d749
Gavaskar <gavaskaraeg@gmail.com>
2024-01-09 19:00:26 +0000

    deleting node in bst

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -25,6 +25,29 @@ public class BST{
             return searchInBst(root.right, val);
           }
         }
+        public static int minInBST(Node root) {
+    return minHelper(root, Integer.MAX_VALUE);
+}
+
+public static int minHelper(Node root, int min) {
+    if (root == null) {
+        return min;
+    }
+
+    if (root.data < min) {
+        min = root.data;
+    }
+
+    // Update min by comparing with the minimum value in the left subtree
+    min = minHelper(root.left, min);
+
+    // Update min by comparing with the minimum value in the right subtree
+    min = minHelper(root.right, min);
+
+    return min;
+}
+
+        
         public static Node inserNode(Node root,int val){
             if(root == null){
               //first node
@@ -48,19 +71,58 @@ public class BST{
         return;
     }
     inOrder(root.left);
-    System.out.println(root.data);
+    System.out.print(root.data+"-"+"");
     inOrder(root.right);
    }
     }
      public static void main(String args[]){
-       int arr[] = {1,2,5,3,4};
+       int arr[] = {5,3,4,1};
   
        Node root = null;
        for(int i = 0;i<arr.length;i++){
         root = Node.inserNode(root,arr[i]);
        }
-      Node.inOrder(root);
-    System.out.println( Node.searchInBst(root, 7)); 
+     // Node.inOrder(root);
+   // System.out.println( Node.searchInBst(root, 1)); 
+   //System.out.println(Node.minInBST(root));
+   Node.inOrder(root);
+   System.out.println("After deleteing the node");
+   Node.delete(root,3);
+   Node.inOrder(root);
     }
-    
+    public static Node delete(Node root, int val){
+      if(root.data > val){
+        root.left = delete(root.left,val);
+      }
+      else if(root.data < val){
+        root.right = delete(root.right,val);
+      }
+      else{
+        //root.data == val
+        //case 1
+        if(root.left == null && root.right == null){
+          return null;
+        }
+        
+        //case 2 
+        if(root.left == null){
+          return root.right;
+        }
+        else if(root.right == null){
+          return root.left;
+        }
+
+        //find the left most node in the right subtree
+        Node IS = inOrderSuccessor(root.right);
+        root.data = IS.data;
+        root.right = delete(root.right,IS.data);
+      }
+      return root;
+    }
+    public static Node inOrderSuccessor(Node root){
+      while(root.left != null){
+        root = root.left;
+      }
+      return root;
+    }
 }
