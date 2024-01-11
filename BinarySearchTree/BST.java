e8db53122fc0ba1c234f9ae688a44a082411cdd3
Gavaskar <gavaskaraeg@gmail.com>
2024-01-11 13:19:26 +0000

    Bst print in range algorithm

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -1,5 +1,5 @@
 package BinarySearchTree;
-
+import java.util.*;
 public class BST{
     static class Node {
         int data;
@@ -112,10 +112,27 @@ public static int minHelper(Node root, int min) {
     }
    
 
+   public static void printInRange(Node root,int x , int y){
+    if(root == null){
+      return;
+    }
+    if(root.data >= x && root.data <= y){
+      printInRange(root.left,x,y);
+      System.out.println(root.data);
+      printInRange(root.right,x,y);
+    }
+    else if(root.data >= y){
+      printInRange(root.left,x,y);
+    }
+    else{
+      printInRange(root.right,x,y);
+    }
+   }
+
 
     }
      public static void main(String args[]){
-       int arr[] = {1,2,3};
+       int arr[] = {1,2,3,4,5,6,7,8,9,10};
   
        Node root = null;
        for(int i = 0;i<arr.length;i++){
@@ -125,8 +142,15 @@ public static int minHelper(Node root, int min) {
    // System.out.println( Node.searchInBst(root, 1)); 
    //System.out.println(Node.minInBST(root));
    Node.inOrder(root);
-   System.out.println("After deleteing the node");
-   Node.delete(root,3);
-   Node.inOrder(root);
+  // System.out.println("After deleteing the node");
+   //Node.delete(root,3);
+   //Node.inOrder(root);
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter Starting Range:");
+   int  x = s.nextInt();
+   System.out.println("Enter End Range:");
+   int y = s.nextInt();
+   System.out.println("printing in Range between"+" "+x+" :"+y);
+   Node.printInRange(root,x,y);
     }
 }
